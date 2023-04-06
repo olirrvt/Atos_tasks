12aecc01d036f78d51343e35bf0b4a7558a66f85
Taylor Oliveira <taylorso2004@hotmail.com>
2023-04-06 11:41:04 -0300

    Start Project

---
+++ listaTarefas2/task04/HorasTrabalhadas.cs
@@ -0,0 +1,67 @@
+﻿namespace HelloWorld;
+class HorasTrabalhadas
+{
+
+    // 4 - Escrever um algoritmo para ler a quantidade de horas aula dadas por dois professores e o valor por hora recebido por cada um deles. 
+    // Mostrar na tela qual dos professores tem o maior salário total.
+
+    static void Main(string[] args)
+    {
+        int horas1, horas2, numMes;
+        double salario1, salario2, total1, total2;
+        string diaDoMes;
+
+        // Lógica do dia do mês para calcular o salário
+
+        Console.WriteLine("Em qual mês você quer calcular o salário do professor?");
+        diaDoMes = Console.ReadLine().ToLower();
+
+        if ((diaDoMes == "abril") || (diaDoMes == "junho") || (diaDoMes == "setembro") || (diaDoMes == "novembro"))
+        {
+            numMes = 30;
+        }
+        else if(diaDoMes == "fevereiro")
+        {
+            numMes = 28;
+        }
+        else
+        {
+            numMes = 31;
+        }
+
+        // Informações do Primeiro Professor.
+
+        Console.WriteLine("Quantas horas o primeiro professor trabalha por dia?");
+        horas1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quanto o primeiro professor ganha por hora?");
+        salario1 = double.Parse(Console.ReadLine());
+
+        total1 = (horas1 * salario1) * numMes;
+
+        // Informações do segundo professor
+        Console.WriteLine("Quantas horas o segundo professor trabalha por dia?");
+        horas2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quanto o segundo professor ganha por hora?");
+        salario2 = double.Parse(Console.ReadLine());
+
+        total2 = (horas2 * salario2) * numMes;
+
+        // Lógica para saber quem ganha mais.
+        
+        if(total1 > total2)
+        {
+            Console.WriteLine($"O primeiro professor ganha mais, no total de R${horas1 * salario1},00 por dia. Totalizando R${total1},00 por mês");
+        }
+        else if(total2 > total1)
+        {
+            Console.WriteLine($"O segundo professor ganha mais, no total de R${horas2 * salario2},00 por dia. Totalizando R${total2},00 por mês");
+
+        }
+        else
+        {
+            Console.WriteLine($"Os dois professores tem salários iguais. Ao total de R${total2},00 por mês");
+        }
+    }
+}
---
+++ listaTarefas2/task05/MediaAlunos.cs
@@ -0,0 +1,64 @@
+﻿namespace HelloWorld;
+
+class MediaAlunos
+{
+
+    // 5 - Escrever um algoritmo para ler duas notas de um aluno e escrever na tela a palavra “Aprovado”. Se a média das duas notas for maior ou igual a 7,0.
+    // Caso a média seja inferior a 7,0, o programa deve ler a nota do exame e calcular a média final,
+    // é a média entre a nota do exame e a média das 2 notas. 
+    // Se esta média final for maior ou igual a 5,0, o programa deve escrever “Aprovado”, caso contrário deve 
+    // escrever “Reprovado”.
+
+    static void Main(string[] args)
+    {
+        float nota1, nota2, media;
+
+        // Informações da segunda nota
+        Console.WriteLine("Digite a sua primeira nota:");
+        nota1 = float.Parse(Console.ReadLine());
+
+        // Informações da segunda nota
+        Console.WriteLine("Digite a sua segunda nota:");
+        nota2 = float.Parse(Console.ReadLine());
+        
+        media = (nota1 + nota2) / 2;
+
+        if (media > 11)
+        {
+            media = media / 10f;
+        } 
+
+        // Lógica da média final
+
+        if( media < 7.0 )
+        {
+            float exameFinal;
+
+            Console.WriteLine($"Sua nota foi {media}.");
+            Console.WriteLine("Você não conseguiu pontos suficientes para passar direto!");
+            Console.WriteLine("Digite a nota do seu exame final:");
+            exameFinal = float.Parse(Console.ReadLine());
+
+            float mediaFinal = ( media + exameFinal) / 3;
+            float mediaArredondada = (float)Math.Round(mediaFinal, 1);
+
+            if( mediaArredondada >= 5.0 )
+            {
+                Console.WriteLine($"Sua nota foi {mediaArredondada}.");
+                Console.WriteLine("Aprovado no exame final!");
+            }
+            else
+            {
+                Console.WriteLine($"Sua nota foi {mediaArredondada}.");
+                Console.WriteLine("Reprovado!");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"Sua nota foi {media.ToString("F1")}.");
+            Console.WriteLine("Parabéns! Aluno aprovado, curta suas férias.");
+        }
+
+    }
+}
